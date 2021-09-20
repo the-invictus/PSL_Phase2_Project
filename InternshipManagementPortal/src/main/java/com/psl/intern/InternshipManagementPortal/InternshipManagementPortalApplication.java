a9b396cecb6203824f23d17d457abbd15ff2b844
rushibot <51736214+rushibot@users.noreply.github.com>
2021-09-20 22:31:13 +0530

    Completed Redirection and list profiles

---
+++ InternshipManagementPortal/src/main/java/com/psl/intern/InternshipManagementPortal/InternshipManagementPortalApplication.java
@@ -0,0 +1,13 @@
+package com.psl.intern.InternshipManagementPortal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InternshipManagementPortalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InternshipManagementPortalApplication.class, args);
+	}
+
+}
---
+++ InternshipManagementPortal/src/test/java/com/psl/intern/InternshipManagementPortal/InternshipManagementPortalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.psl.intern.InternshipManagementPortal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InternshipManagementPortalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
